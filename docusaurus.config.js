fcc763c49121b13e734aa27b1889673d19461dd0
Nicolas <nicolas.lecrique+git@globers.co>
2021-10-29 17:40:24 +0200

    fix link to github doc markdown files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,13 +25,7 @@ const config = {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+          editUrl: 'https://github.com/datatoggle/datatoggle-doc/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
